6e806d729ce1fec406e8b39a64666692b21278d8
eliud wainaina <wainainaeliud7@gmail.com>
2024-04-16 09:39:15 -0700

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+function mondayWork(job = "go to the office") {
+    return `This Monday, I will ${job}.`;
+}
+
+function wrapAdjective(sense ="*"){
+    return function(adjective){
+        return `You are ${sense} ${adjective}${sense}!`;
+    }
+}
